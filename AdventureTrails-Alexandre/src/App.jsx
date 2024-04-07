b5af27f94ff7492a3648cefcc85235ae0e7485b8
Alexandre_B_Basso <alexandrebbasso@gmail.com>
2024-04-07 19:52:19 -0300

    AdventureTrails-Alexandre

---
+++ AdventureTrails-Alexandre/src/App.jsx
@@ -0,0 +1,13 @@
+import CardTrilha from "./components/CardTrilha.jsx"
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello</h1>
+      <CardTrilha />
+    </>
+  )
+}
+
+export default App
---
+++ AdventureTrails-Alexandre/src/components/CardTrilha.jsx
@@ -0,0 +1,18 @@
+export default function CardTrilha(props){
+
+
+    return (
+    <>
+     <h3>{props.nome}</h3>
+     <h3></h3>
+     <h3></h3>
+     <h3></h3>
+     <h3></h3>
+     <h3></h3>
+     <h3></h3>
+     <h3></h3>
+    </>
+
+
+    )
+}
